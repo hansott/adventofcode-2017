fd2f807424bc3ce1b41dd922993cb022b4345d95
Hans Ott <hansott@hotmail.be>
2017-12-02 20:26:49 +0100

    ✨ Add solution for part 2 of day 2

--- 02-CorruptionChecksum.hs
+++ 02-CorruptionChecksum.hs
@@ -10,7 +10,18 @@ toSpreadsheet input = init $ map (\x -> map toInt x) $ map (\x -> splitOn "\t" x
 solve :: String -> Int
 solve input = sum $ map diff $ toSpreadsheet input
 
+evenPair :: [Int] -> (Int, Int)
+evenPair row = head [ (a, b) | a <- row, b <- row, a /= b, even $ rem a b, mod a b == 0 ]
+
+evenQuotient :: [Int] -> Int
+evenQuotient row = quot (fst pair) (snd pair)
+    where pair = evenPair row
+
+solve2 :: String -> Int
+solve2 input = sum $ map evenQuotient $ toSpreadsheet input
+
 main :: IO ()
 main = do
     contents <- readFile "02-CorruptionCheckSum.txt"
     putStrLn $ show $ solve contents
+    putStrLn $ show $ solve2 contents
