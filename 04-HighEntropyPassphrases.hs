4e5280857c7c5c2d26846d1cd30dc1545816e6f2
Hans Ott <hansott@hotmail.be>
2017-12-04 12:15:25 +0100

    ✨ Add solution part 2 for day 4

--- 04-HighEntropyPassphrases.hs
+++ 04-HighEntropyPassphrases.hs
@@ -6,11 +6,24 @@ hasUniqueWords :: Passphrase -> Bool
 hasUniqueWords passphrase = (length $ wordsOfPassphrase) == (length $ nub $ wordsOfPassphrase)
     where wordsOfPassphrase = words passphrase
 
+isAnagram :: (String, String) -> Bool
+isAnagram (a, b) = length a == length b && sort a == sort b
+
+noAnagrams :: Passphrase -> Bool
+noAnagrams passphrase = length (filter isAnagram combinations) == 0
+    where wordsPass = words passphrase
+          combinations = [ (a, b) | a <- wordsPass, b <- wordsPass, a /= b ]
+
 passphrasePolicy1 :: Passphrase -> Bool
 passphrasePolicy1 = hasUniqueWords
+
+passphrasePolicy2 :: Passphrase -> Bool
+passphrasePolicy2 passphrase = hasUniqueWords passphrase && noAnagrams passphrase
 
 main :: IO ()
 main = do
     contents <- readFile "04-HighEntropyPassphrases.txt"
-    validPasshprases <- return $ filter passphrasePolicy1 $ lines contents
-    putStrLn $ "There are " ++ (show $ length validPasshprases) ++ " valid passphrase(s) for policy 1"
+    validPasshprasesPolicy1 <- return $ filter passphrasePolicy1 $ lines contents
+    putStrLn $ "There are " ++ (show $ length validPasshprasesPolicy1) ++ " valid passphrases for policy 1"
+    validPassphrasesPolicy2 <- return $ filter passphrasePolicy2 $ lines contents
+    putStrLn $ "There are " ++ (show $ length validPassphrasesPolicy2) ++ " valid passphrases for policy 2"
