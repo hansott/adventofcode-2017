0d90410fee4b1b2a18554f9dea94b1e59dd6f189
Hans Ott <hansott@hotmail.be>
2017-12-09 12:39:18 +0100

    ✨ Add solution for part 2 of day 5
    The programs ends with a stack overflow. Yeah, I know right. I'm clearly
    doing something wrong but my Haskell skills at the moment can't help me
    figure out why.

--- 05-MazeOfTrampolines.hs
+++ 05-MazeOfTrampolines.hs
@@ -5,13 +5,19 @@ data State = State { instructions :: [Instruction], index :: Int } deriving (Sho
 isValidIndex :: State -> Int -> Bool
 isValidIndex state index = index >= 0 && index < (length $ instructions state)
 
-increment :: [Instruction] -> Int -> [Instruction]
-increment instructions index = take index instructions ++ [nextValue] ++ drop (index + 1) instructions
+increment1 :: [Instruction] -> Int -> [Instruction]
+increment1 instructions index = take index instructions ++ [nextValue] ++ drop (index + 1) instructions
     where currentValue = instructions !! index
           nextValue = currentValue + 1
 
-followInstruction :: State -> State
-followInstruction state =
+increment2 :: [Instruction] -> Int -> [Instruction]
+increment2 instructions index = take index instructions ++ [nextValue] ++ drop (index + 1) instructions
+    where currentValue = instructions !! index
+          nextValue = if currentValue >= 3 then currentValue - 1 else currentValue + 1
+
+
+followInstruction :: State -> ([Instruction] -> Int -> [Instruction]) -> State
+followInstruction state increment =
     if isValidIndex state current
     then State { instructions = increment inst current, index = current + (inst !! current) }
     else state
@@ -24,16 +30,19 @@ toInstructions input = map (\x -> read x :: Instruction) $ lines input
 createState :: [Instruction] -> State
 createState instructions = State { instructions = instructions, index = 0 }
 
-getStepsTillOutOfBounds :: State -> Int -> Int
-getStepsTillOutOfBounds state steps =
+getStepsTillOutOfBounds :: (State, Int) -> ([Instruction] -> Int -> [Instruction]) -> (State, Int)
+getStepsTillOutOfBounds state steps increment =
     if isValidIndex state current
-    then getStepsTillOutOfBounds (followInstruction state) (steps + 1)
+    then getStepsTillOutOfBounds (followInstruction state increment) (steps + 1) increment
     else steps
     where current = index state
 
-getStepsTillOutOfBounds' state = getStepsTillOutOfBounds state 0
+getStepsTillOutOfBounds1 state = getStepsTillOutOfBounds state 0 increment1
+
+getStepsTillOutOfBounds2 state = getStepsTillOutOfBounds state 0 increment2
 
 main :: IO ()
 main = do
     contents <- readFile "05-MazeOfTrampolines.txt"
-    putStrLn $ show $ getStepsTillOutOfBounds' $ createState $ toInstructions contents
+    putStrLn $ show $ getStepsTillOutOfBounds1 $ createState $ toInstructions contents
+    putStrLn $ show $ getStepsTillOutOfBounds2 $ createState $ toInstructions contents
